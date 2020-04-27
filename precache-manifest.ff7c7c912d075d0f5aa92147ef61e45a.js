5d587de486fb2cc25e6554368fb7e517072f08b6
MusicJunkyREZ <reidzuckerman@gmail.com>
2020-04-26 18:06:22 -0600

    Updates

---
+++ precache-manifest.ff7c7c912d075d0f5aa92147ef61e45a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5333a018e117e3b23bcd54bb81b271e1",
+    "url": "/casosdecoronavirus2/index.html"
+  },
+  {
+    "revision": "bcc5362b8e5a70eac500",
+    "url": "/casosdecoronavirus2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "af99057280abc9175aca",
+    "url": "/casosdecoronavirus2/static/js/2.07e6e117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/casosdecoronavirus2/static/js/2.07e6e117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc5362b8e5a70eac500",
+    "url": "/casosdecoronavirus2/static/js/main.60fbce4a.chunk.js"
+  },
+  {
+    "revision": "9f900cd7ca2f3fabf10a",
+    "url": "/casosdecoronavirus2/static/js/runtime-main.5ce2556e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/casosdecoronavirus2/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/casosdecoronavirus2/precache-manifest.ff7c7c912d075d0f5aa92147ef61e45a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/casosdecoronavirus2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
